c62cd32a7dcd4ae9becee312f5211d5d3789986d
Abdel Aziz NGOUH <Abdel Aziz NGOUH@NDJAMA-PC.connectify>
2015-12-26 12:27:35 +0100

    abdel aziz c'est mon Nom!!!!!!!!!!!!!!!!

---
+++ FoodTest/src/main/java/com/mycompany/foodtest/qdcboncbon.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.foodtest;
+
+/**
+ *
+ * @author Abdel Aziz NGOUH
+ */
+public class qdcboncbon {
+    public static void main(String[] args) {
+        System.out.println("s'il suffit de réfléchir pour dévenir riche, alors nous allons être riche les gars!!!!! ");
+    }
+    
+}
